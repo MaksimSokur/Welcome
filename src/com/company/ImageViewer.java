cf2c2ae43e2fc0b88d44d54ef71eb34c05530ed3
Maksim <maksim.sokur90@gmail.com>
2015-06-18 21:42:56 +0300

    Simple project

---
+++ src/com/company/ImageViewer.java
@@ -0,0 +1,71 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+class Main {
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JFrame frame = new ImageViewerFrame();
+                frame.setTitle("Image Viewer");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setVisible(true);
+            }
+        });
+	// write your code here
+    }
+}
+
+class ImageViewerFrame extends JFrame {
+    private JLabel label;
+    private JFileChooser chooser;
+    private static final int DEFAULT_WIDTH = 300;
+    private static final int DEFAULT_HEIGHT = 400;
+
+    public ImageViewerFrame() {
+        setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+        label = new JLabel();
+        add(label);
+
+        //Установить селектор файлов
+        chooser = new JFileChooser();
+        chooser.setCurrentDirectory(new File("."));
+
+        //Установить стрку меню
+        JMenuBar menuBar = new JMenuBar();
+        setJMenuBar(menuBar);
+
+        JMenu menu = new JMenu("File");
+        menuBar.add(menu);
+
+        JMenuItem openItem = new JMenuItem("Open");
+        menu.add(openItem);
+        openItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                //отобразить диалоговое окно селектора файлов
+                int result = chooser.showOpenDialog(null);
+
+                if (result == JFileChooser.APPROVE_OPTION) {
+                    String name = chooser.getSelectedFile().getPath();
+                    label.setIcon(new ImageIcon(name));
+                }
+            }
+        });
+
+        JMenuItem exitItem = new JMenuItem("Exit");
+        menu.add(exitItem);
+        exitItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                System.exit(0);
+            }
+        });
+    }
+}
